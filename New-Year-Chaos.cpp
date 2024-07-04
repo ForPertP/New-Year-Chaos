de36f002b0010356b0c2df4415f4dbe89ee01128
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-07-05 06:09:24 +0900

    Update New-Year-Chaos.cpp

--- New-Year-Chaos.cpp
+++ New-Year-Chaos.cpp
@@ -14,37 +14,30 @@ vector<string> split(const string &);
 
 void minimumBribes(vector<int> q)
 {
-    int total = 0;
-    int test = 0;
+    int totalBribes = 0;
     int n = q.size();
-    
-    for (size_t i = 0; i < q.size(); ++i)
-    {
-        int check = q[i] - (i + 1);
 
-        if (check > 2)
+    for (int i = 0; i < n; ++i)
+    {
+        int originalPosition = q[i] - 1;
+        int currentPosition = i;
+
+        if (originalPosition - currentPosition > 2)
         {
-            total = -1;
-            break;
+            cout << "Too chaotic" << '\n';
+            return;
         }
 
-        for (size_t j = std::max(0, q[i] - 2); j < i; ++j)
+        for (int j = max(0, originalPosition - 1); j < currentPosition; ++j)
         {
             if (q[j] > q[i])
             {
-                total++;
+                totalBribes++;
             }
         }
     }
 
-    if (total == -1)
-    {
-        cout << "Too chaotic" << '\n';
-    }
-    else
-    {
-        cout << total << '\n';
-    }
+    cout << totalBribes << '\n';
 }
 
 
@@ -118,7 +111,3 @@ vector<string> split(const string &str) {
 
     return tokens;
 }
-    
-    return 0;
-}
-
