21633e37b983ab5aa2b4c013bf46a8edf7ee7617
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-07-08 05:37:58 +0900

    Update New-Year-Chaos.java

--- New-Year-Chaos.java
+++ New-Year-Chaos.java
@@ -1,1 +1,27 @@
-New-Year-Chaos
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumBribes' function below.
+     *
+     * The function accepts INTEGER_ARRAY q as parameter.
+     */
+
+    public static void minimumBribes(List<Integer> q) {
+        int totalBribes = 0;
+
+        System.out.println(totalBribes);
+    }    
+
+}
