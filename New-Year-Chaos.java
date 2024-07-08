e34fc95f99425e0786850f2a8c89e5d2de1d37be
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-07-09 03:44:10 +0900

    Update New-Year-Chaos.java

--- New-Year-Chaos.java
+++ New-Year-Chaos.java
@@ -25,3 +25,29 @@ class Result {
     }    
 
 }
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                Result.minimumBribes(q);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+}
